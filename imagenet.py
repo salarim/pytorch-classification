3a81ed7eebf3b934037794ff4e0f90ec91628950
Mohammad Hadi Salari <msalari@cedar1.cedar.computecanada.ca>
2020-05-24 16:40:28 -0700

    Revert "Change weight_decay"
    This reverts commit f906a442d13efe7320bc0a1ffbb0b32906594380.

--- imagenet.py
+++ imagenet.py
@@ -65,8 +65,8 @@ parser.add_argument('--schedule', type=int, nargs='+', default=[150, 225],
 parser.add_argument('--gamma', type=float, default=0.1, help='LR is multiplied by gamma on schedule.')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
-parser.add_argument('--weight-decay', '--wd', default=5e-4, type=float,
-                    metavar='W', help='weight decay (default: 5e-4)')
+parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)')
 # Checkpoints
 parser.add_argument('-c', '--checkpoint', default='checkpoint', type=str, metavar='PATH',
                     help='path to save checkpoint (default: checkpoint)')
